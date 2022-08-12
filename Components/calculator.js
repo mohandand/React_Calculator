035ff174e6d7594299fa88007b3cb22b33a1f934
mohandand <mohandand@gmail.com>
2022-08-12 13:39:47 -0700

    1 file modified

--- Components/calculator.js
+++ Components/calculator.js
@@ -2,10 +2,34 @@ import React, { useState } from 'react';
 import './calculator.css';
 
 export default function Calculator() {
+  const [calc, setCalc] = useState('');
+  const [result, setResult] = useState('');
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+  function updateCalc(value) {
+    if (
+      (ops.includes(value) && calc == '') ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+  }
+
   function createDigits() {
     let digits = [];
     for (let i = 1; i < 10; i++) {
-      digits.push(<button key={i}>{i}</button>);
+      digits.push(
+        <button
+          key={i}
+          onClick={() => {
+            updateCalc(i.toString());
+          }}
+        >
+          {i}
+        </button>
+      );
     }
     return digits;
   }
@@ -14,21 +38,64 @@ export default function Calculator() {
     <div className="App">
       <div className="container">
         <div className="display">
-          <span>(0)</span>0
+          {result ? <span>(0)</span> : ''}
+          {calc || 0}
         </div>
         <div className="operators">
-          <button>/</button>
-          <button>*</button>
-          <button>+</button>
-          <button>-</button>
+          <button
+            onClick={() => {
+              updateCalc('/');
+            }}
+          >
+            /
+          </button>
+          <button
+            onClick={() => {
+              updateCalc('*');
+            }}
+          >
+            *
+          </button>
+          <button
+            onClick={() => {
+              updateCalc('+');
+            }}
+          >
+            +
+          </button>
+          <button
+            onClick={() => {
+              updateCalc('-');
+            }}
+          >
+            -
+          </button>
 
           <button>DEL</button>
         </div>
         <div className="digits">
           {createDigits()}
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <button
+            onClick={() => {
+              updateCalc('0');
+            }}
+          >
+            0
+          </button>
+          <button
+            onClick={() => {
+              updateCalc('.');
+            }}
+          >
+            .
+          </button>
+          <button
+            onClick={() => {
+              updateCalc('=');
+            }}
+          >
+            =
+          </button>
         </div>
       </div>
     </div>
